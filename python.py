965223e099cbef5e690a848e47ab964089c44fe9
Diorbek <136899452+DiorbekKoroche@users.noreply.github.com>
2023-06-23 10:06:25 +0500

    Add files via upload

--- python.py
+++ python.py
@@ -1,26 +1,26 @@
 from tkinter import *
 root = Tk()
-root.geometry("500x300")
+root.geometry("400x200")
 
 def getvals():
     print("Accepted")
 
 # Heading
-Label(root, text="REGIATRATION FORM", font="arial 15 bold").grid(row=0, column=3)
+Label(root, text="REGIATRATION FORM", font="arial 15 bold").grid(row=0, column=4)
 
 # Field Name
-name = Label(root, text="Name")
-phone = Label(root, text="Phone Number")
-gender = Label(root, text="Gender")
-emergency = Label(root, text="Emergency")
-paymentmood = Label(root, text="Payment Mood")
+name = Label(root, text="Name", font="arial 10 bold")
+phone = Label(root, text="Phone Number", font="arial 10 bold")
+gender = Label(root, text="Gender", font="arial 10 bold")
+emergency = Label(root, text="Emergency", font="arial 10 bold")
+paymentmood = Label(root, text="Payment Mood", font="arial 10 bold")
 
 # Packing Fields
-name.grid(row=1, column=2)
-phone.grid(row=2, column=2)
-gender.grid(row=3, column=2)
-emergency.grid(row=4, column=2)
-paymentmood.grid(row=5, column=2)
+name.grid(row=1, column=3)
+phone.grid(row=2, column=3)
+gender.grid(row=3, column=3)
+emergency.grid(row=4, column=3)
+paymentmood.grid(row=5, column=3)
 
 # Variable for storing data
 namevalue = StringVar
@@ -38,17 +38,17 @@ emergencyentry = Entry(root, textvariable =emergencyvalue)
 paymentmoodentry = Entry(root, textvariable =paymentmoodvalue)
 
 # Packing entry fields
-nameentry.grid(row=1, column=3)
-phoneentry.grid(row=2, column=3)
-genderentry.grid(row=3, column=3)
-emergencyentry.grid(row=4, column=3)
-paymentmoodentry.grid(row=5, column=3)
+nameentry.grid(row=1, column=4)
+phoneentry.grid(row=2, column=4)
+genderentry.grid(row=3, column=4)
+emergencyentry.grid(row=4, column=4)
+paymentmoodentry.grid(row=5, column=4)
 
 # Creating Checkbox
 checkbtn = Checkbutton(text="remember me?", variable = checkvalue)
-checkbtn.grid(row=6, column=3)
+checkbtn.grid(row=6, column=4)
 
 # Submit button
-Button(text="Submit", command=getvals).grid(row=7, column=3)
+Button(text="Submit", command=getvals).grid(row=7, column=4)
 
 root.mainloop()
